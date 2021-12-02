96fe1afc834aaad796be9eef3fc2572ec3836ec8
vazqjose <vazqjose@gmail.com>
2021-12-02 19:57:52 -0400

    added material list module

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { ViewUserComponent } from './view-user/view-user.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    ListUsersComponent,
+    ViewUserComponent,
+    AddUserComponent,
+    EditUserComponent,
+    DeleteUserComponent
+  ],
+  imports: [
+    CommonModule,
+    MatListModule,
+    RouterModule
+  ]
+})
+export class UsersModule { }
