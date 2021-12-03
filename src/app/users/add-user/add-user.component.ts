ce8d165a3c2f4035e30fa7e243926b6fdf90bdb7
vazqjose <vazqjose@gmail.com>
2021-12-03 00:14:36 -0400

    Added createuser method that calls user service.

--- src/app/users/add-user/add-user.component.ts
+++ src/app/users/add-user/add-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-add-user',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+  /*
+    new formgroup instance for adding user data
+  */
+  addUserForm: FormGroup = new FormGroup({});
 
+  /*
+    create instance of user service and snackbar material component
+  */
+  constructor(private formBuilder: FormBuilder,
+    private userService: UserService,
+    private _snackBar: MatSnackBar) {}
+
+  /*
+    set our form data components
+  */
   ngOnInit(): void {
+    this.addUserForm = this.formBuilder.group({
+      'username': new FormControl(''),
+      'email': new FormControl(''),
+      'phone': new FormControl('')
+    })
+  }
+
+  /*
+    call addUser method in user service, send form objects.  if post method
+    was successful or not, inform the user with a popup message
+  */
+  createUser() {
+    this.userService.addUser(this.addUserForm.value).subscribe(data => {
+        this._snackBar.open("User created successfully")
+    }, err => {
+      this._snackBar.open("Could not create user");
+    });
   }
 
 }
