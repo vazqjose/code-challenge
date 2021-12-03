4908a7c23eefba810c103ea29fe750256f4e74c4
vazqjose <vazqjose@gmail.com>
2021-12-03 15:57:53 -0400

    scss sample vars and mixin starting file

--- src/app/users/list-users/list-users.component.ts
+++ src/app/users/list-users/list-users.component.ts
@@ -12,16 +12,28 @@ export class ListUsersComponent implements OnInit {
     Create our data recipient and service of type UserService
   */
   listUsers: any;
+  get_users: any;
   constructor(private userService: UserService) { }
 
   /*
-    Call method listUsers in our service to obtain all users, use subscribe
+    Call  listUsers method in our service to obtain all users, use subscribe
     directive to handle observable data type
   */
-  ngOnInit(): void {
-    this.userService.listUsers().subscribe(data => {
+  ngOnInit(): void
+  {
+    this.userService.listUsers().subscribe(
+      data => {
         this.listUsers = data;
-    });
+
+        // save to local storage
+        localStorage.setItem("list_users", JSON.stringify(this.listUsers));
+        this.get_users = localStorage.getItem("list_users");
+        console.log(this.get_users);
+      },
+      err => console.error(err),
+      () => console.log('Users loaded')
+    );
   }
 
 }
+
