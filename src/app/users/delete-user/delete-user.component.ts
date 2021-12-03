d1e9ca2b1209e6d27d92a6a4731ba12df4420ec5
vazqjose <vazqjose@gmail.com>
2021-12-03 00:20:51 -0400

    completed parameter validation, return message and delete request

--- src/app/users/delete-user/delete-user.component.ts
+++ src/app/users/delete-user/delete-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-delete-user',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteUserComponent implements OnInit {
 
-  constructor() { }
+  userId: string = '';
 
+  /*
+    call material modules and user service
+  */
+  constructor(private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private _snackBar: MatSnackBar,
+    private router: Router) {}
+
+    /*
+      receive user id from observable object
+    */
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(data => {
+      this.userId = data['id'];
+    });
+
+    /*
+      validate if parameter is sent.  call deleteUser method in user service
+      otherwise, capture and display error. then, navigate back to listed users.
+    */
+    if (this.userId){
+      this.userService.deleteUser(this.userId).subscribe(data => {
+          this._snackBar.open("User deleted");
+      }, err => {
+        this._snackBar.open("Error deleting user");
+      })
+      this.router.navigate(['users']);
+    }
   }
 
 }
