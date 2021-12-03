c1ddd660fa367a59f2ef417cfac6e132db62e8a2
vazqjose <vazqjose@gmail.com>
2021-12-03 00:16:59 -0400

    Imported material modules for ui components and form field module

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -7,7 +7,13 @@ import { EditUserComponent } from './edit-user/edit-user.component';
 import { DeleteUserComponent } from './delete-user/delete-user.component';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
     ListUsersComponent,
@@ -19,7 +25,17 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     MatListModule,
-    RouterModule
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule
+  ],
+  providers: [
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}
   ]
 })
 export class UsersModule { }
